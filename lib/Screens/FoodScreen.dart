f6234e8b79f8c2bcfae70212c57d30ce836011a1
jyothir10 <j.adithyan16@gmail.com>
2022-06-26 21:00:27 +0530

    FoodScreen.dart

---
+++ lib/Screens/FoodScreen.dart
@@ -0,0 +1,386 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FoodScreen extends StatefulWidget {
+  static const String id = '/food';
+  const FoodScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FoodScreen> createState() => _FoodScreenState();
+}
+
+class _FoodScreenState extends State<FoodScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * .166),
+          child: AppBar(
+            leading: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.arrow_back_outlined,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * .05),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.search_rounded,
+                      ),
+                    ),
+                    const Text(
+                        "Search",
+                        style: TextStyle(
+                            color:  Color(0xffffffff),
+                            fontWeight: FontWeight.w400,
+                            fontFamily: "SFUIText",
+                            fontStyle:  FontStyle.normal,
+                            fontSize: 12.3
+                        ),
+                        textAlign: TextAlign.left
+                    ),
+                  ],
+                ),
+              ),
+            ],
+            flexibleSpace: const Image(
+              image: AssetImage("images/bg.png"),
+                fit: BoxFit.cover,
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .05),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:[
+                    // Recommended (14)
+                    const Text(
+                        "Recommended (14)",
+                        style: TextStyle(
+                            color:  Color(0xff000000),
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "SFUIText",
+                            fontStyle:  FontStyle.normal,
+                            fontSize: 12.0
+                        ),
+                        textAlign: TextAlign.left
+                    ),
+                    Card(
+                      color: const Color(0xff042e60),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Row(
+                          children: [
+                            // Menu
+                            const Text(
+                                "Menu ",
+                                style: TextStyle(
+                                    color:  Color(0xffffffff),
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: "SFUIText",
+                                    fontStyle:  FontStyle.normal,
+                                    fontSize: 9.2
+                                ),
+                                textAlign: TextAlign.left
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: const Icon(
+                                Icons.file_open_outlined,
+                                size: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Card(
+                  elevation: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Image(
+                            image: AssetImage("images/fd.png"),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * .05),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:  [
+                                // Zinger Burger
+                                Text(
+                                    "Zinger Burger",
+                                    style: TextStyle(
+                                        color:  Color(0xff000000),
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 17.5
+                                    ),
+                                    textAlign: TextAlign.left
+                                ),
+                                // ₹ 179
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * .02),
+                                  child: const Text(
+                                      "₹ 179",
+                                      style: TextStyle(
+                                          color:  Color(0xff000000),
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: "SFUIText",
+                                          fontStyle:  FontStyle.normal,
+                                          fontSize: 12.0
+                                      ),
+                                      textAlign: TextAlign.left
+                                  ),
+                                ),
+                                // French Fires
+                                Text(
+                                    "French Fires",
+                                    style: TextStyle(
+                                        color:  const Color(0xff000000),
+                                        fontWeight: FontWeight.w500,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 10.0
+                                    ),
+                                    textAlign: TextAlign.left
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Icon(
+                        Icons.add_circle,
+                        color: Color(0xffffb800),
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  elevation: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Image(
+                            image: AssetImage("images/fd.png"),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * .05),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:  [
+                                // Zinger Burger
+                                Text(
+                                    "Zinger Burger",
+                                    style: TextStyle(
+                                        color:  Color(0xff000000),
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 17.5
+                                    ),
+                                    textAlign: TextAlign.left
+                                ),
+                                // ₹ 179
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * .02),
+                                  child: const Text(
+                                      "₹ 179",
+                                      style: TextStyle(
+                                          color:  Color(0xff000000),
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: "SFUIText",
+                                          fontStyle:  FontStyle.normal,
+                                          fontSize: 12.0
+                                      ),
+                                      textAlign: TextAlign.left
+                                  ),
+                                ),
+                                // French Fires
+                                Text(
+                                    "French Fires",
+                                    style: TextStyle(
+                                        color:  const Color(0xff000000),
+                                        fontWeight: FontWeight.w500,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 10.0
+                                    ),
+                                    textAlign: TextAlign.left
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Icon(
+                        Icons.add_circle,
+                        color: Color(0xffffb800),
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  elevation: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Image(
+                            image: AssetImage("images/fd.png"),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * .05),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:  [
+                                // Zinger Burger
+                                Text(
+                                    "Zinger Burger",
+                                    style: TextStyle(
+                                        color:  Color(0xff000000),
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 17.5
+                                    ),
+                                    textAlign: TextAlign.left
+                                ),
+                                // ₹ 179
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * .02),
+                                  child: const Text(
+                                      "₹ 179",
+                                      style: TextStyle(
+                                          color:  Color(0xff000000),
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: "SFUIText",
+                                          fontStyle:  FontStyle.normal,
+                                          fontSize: 12.0
+                                      ),
+                                      textAlign: TextAlign.left
+                                  ),
+                                ),
+                                // French Fires
+                                Text(
+                                    "French Fires",
+                                    style: TextStyle(
+                                        color:  const Color(0xff000000),
+                                        fontWeight: FontWeight.w500,
+                                        fontFamily: "SFUIText",
+                                        fontStyle:  FontStyle.normal,
+                                        fontSize: 10.0
+                                    ),
+                                    textAlign: TextAlign.left
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Icon(
+                        Icons.add_circle,
+                        color: Color(0xffffb800),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+// Container(
+// color: Colors.white,
+// child: Container(
+// color: Color(0xff042e60),
+// width: MediaQuery.of(context).size.width * .9,
+// height: MediaQuery.of(context).size.width * .087,
+// child: Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// Column(
+// children: [
+// // 1 Item
+// Text(
+// "1 Item",
+// style: TextStyle(
+// color:  Color(0xffffffff),
+// fontWeight: FontWeight.w400,
+// fontFamily: "SFUIText",
+// fontStyle:  FontStyle.normal,
+// fontSize: 9.8
+// ),
+// textAlign: TextAlign.left
+// ),
+// // ₹ 179
+// Row(
+// children: const [
+// Text(
+// "₹ 179 ",
+// style: TextStyle(
+// color:  Color(0xffffffff),
+// fontWeight: FontWeight.w600,
+// fontFamily: "SFUIText",
+// fontStyle:  FontStyle.normal,
+// fontSize: 14.6
+// ),
+// textAlign: TextAlign.left
+// ),
+// // plus taxes
+// Text(
+// "plus taxes",
+// style: TextStyle(
+// color:  Color(0xffffffff),
+// fontWeight: FontWeight.w400,
+// fontFamily: "SFUIText",
+// fontStyle:  FontStyle.normal,
+// fontSize: 7.3
+// ),
+// textAlign: TextAlign.left
+// )
+// ],
+// )
+// ],
+// ),
+// // View Cart
+// const Text(
+// "View Cart",
+// style: TextStyle(
+// color:  Color(0xffffffff),
+// fontWeight: FontWeight.w600,
+// fontFamily: "SFUIText",
+// fontStyle:  FontStyle.normal,
+// fontSize: 12.2
+// ),
+// textAlign: TextAlign.left
+// )
+// ],
+// ),
+// ),
+// ),
