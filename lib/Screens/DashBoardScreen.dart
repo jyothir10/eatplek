7f6e163b8b7591844f4c3cbcfde8532f63a5c4f2
sanika2001 <porayilsanika@gmail.com>
2022-06-29 00:20:27 +0530

    Extracted app bar

--- lib/Screens/DashBoardScreen.dart
+++ lib/Screens/DashBoardScreen.dart
@@ -14,94 +14,18 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
-  int d = 0, t = 0, veg = 1, ac = 0, type = 0;
+  int d = 1, t = 0, veg = 1, ac = 0, type = 0;
   bool? vegcheck = false, noncheck = false, accheck = false, nonaccheck = false;
+  var items = ['Home', 'Office'];
+  String dropdownvalue = 'Home';
   @override
   Widget build(BuildContext context) {
-    var items = ['Home', 'Office'];
-    String dropdownvalue = 'Home';
     String address = 'Mc Hostel, Aramana Road, Chengannur, Keral...';
     return Scaffold(
       bottomNavigationBar: const BottomBar(
         index: 0,
       ),
-      appBar: PreferredSize(
-        preferredSize:
-            Size.fromHeight(MediaQuery.of(context).size.height * .09),
-        child: SafeArea(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            // height: MediaQuery.of(context).size.height * .09 ,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(18),
-                  bottomLeft: Radius.circular(18)),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0x30000000),
-                  offset: Offset(0, -2),
-                  blurRadius: 19,
-                  spreadRadius: -1,
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 5),
-                          child: Image.asset(
-                            "images/location.png",
-                            height: 20,
-                          ),
-                        ),
-                        DropdownButtonHideUnderline(
-                          child: DropdownButton(
-                              value: dropdownvalue,
-                              style: const TextStyle(
-                                color: Color(0xff1d1d1d),
-                                fontSize: 14.133333206176758,
-                                fontFamily: 'SFUIText',
-                                fontWeight: FontWeight.w700,
-                              ),
-                              items: items.map((String items) {
-                                return DropdownMenuItem(
-                                  value: items,
-                                  child: Text(items),
-                                );
-                              }).toList(),
-                              onChanged: (String? value) {
-                                setState(() {
-                                  dropdownvalue = value!;
-                                });
-                              }),
-                        )
-                      ],
-                    ),
-                    Text(
-                      address,
-                      style: const TextStyle(
-                        color: Color(0xff1d1d1d),
-                        fontSize: 10,
-                        fontFamily: 'SFUIText',
-                      ),
-                    ),
-                  ],
-                ),
-                Image.asset("images/search.png"),
-              ],
-            ),
-          ),
-        ),
-      ),
+      appBar: buildAppBar(context, address), //look at bottom for code
       body: Container(
         color: Colors.white,
         width: MediaQuery.of(context).size.width,
@@ -531,6 +455,85 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                 )
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  PreferredSize buildAppBar(BuildContext context, String address) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * .09),
+      child: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          // height: MediaQuery.of(context).size.height * .09 ,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x30000000),
+                offset: Offset(0, -2),
+                blurRadius: 19,
+                spreadRadius: -1,
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Image.asset(
+                          "images/location.png",
+                          height: 20,
+                        ),
+                      ),
+                      DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                            value: dropdownvalue,
+                            style: const TextStyle(
+                              color: Color(0xff1d1d1d),
+                              fontSize: 14.133333206176758,
+                              fontFamily: 'SFUIText',
+                              fontWeight: FontWeight.w700,
+                            ),
+                            items: items.map((String items) {
+                              return DropdownMenuItem(
+                                value: items,
+                                child: Text(items),
+                              );
+                            }).toList(),
+                            onChanged: (String? value) {
+                              setState(() {
+                                dropdownvalue = value!;
+                              });
+                            }),
+                      )
+                    ],
+                  ),
+                  Text(
+                    address,
+                    style: const TextStyle(
+                      color: Color(0xff1d1d1d),
+                      fontSize: 10,
+                      fontFamily: 'SFUIText',
+                    ),
+                  ),
+                ],
+              ),
+              Image.asset("images/search.png"),
+            ],
           ),
         ),
       ),
