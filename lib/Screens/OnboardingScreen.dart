a7deb494c0924c244ba54e2f3b7a36e474182d1f
sanika2001 <porayilsanika@gmail.com>
2022-07-07 22:26:17 +0530

    Onboarding

--- lib/Screens/OnboardingScreen.dart
+++ lib/Screens/OnboardingScreen.dart
@@ -1,7 +1,6 @@
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:toast/toast.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
 import 'LoginScreen.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -12,27 +11,27 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  @override
-  void _onIntroEnd(context) {
-    //todo:add navigation
-  }
   DateTime? currentBackPressTime;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   Future<bool> onWillPop() {
     DateTime now = DateTime.now();
     if (currentBackPressTime == null ||
         now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
       currentBackPressTime = now;
-
-      Toast.show("Press back again to exit",
-          duration: Toast.lengthShort, gravity: Toast.bottom);
+      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 1),
+          content: Text("Press back again to exit")));
       return Future.value(false);
     }
     return Future.value(true);
   }
 
+  @override
   Widget build(BuildContext context) {
     return WillPopScope(
+      key: _scaffoldKey,
       onWillPop: onWillPop,
       child: EasySplashScreen(
         showLoader: false,
