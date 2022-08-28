bb54818d24f8b32b9920b44b96424ef0d74a24df
sanika2001 <porayilsanika@gmail.com>
2022-08-28 23:20:45 +0530

    Push notification in 5 seconds

--- lib/Components/NotificationApi.dart
+++ lib/Components/NotificationApi.dart
@@ -1,38 +1,70 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:timezone/timezone.dart' as tz;
 
-class NotificationApi{
-
-
-
+class NotificationApi {
   static final _notifications = FlutterLocalNotificationsPlugin();
+  static final onNotifications = BehaviorSubject<String?>();
 
   static Future _notificationDetails() async {
-
     var initializationSettingsAndroid =
-    new AndroidInitializationSettings('@mipmap/ic_launcher');
+        new AndroidInitializationSettings('ic_notif');
 
-    print("222");
-    return NotificationDetails(
+    return const NotificationDetails(
       android: AndroidNotificationDetails(
         'channel id',
         'channel name',
-        icon: "ic_launcher",
+        icon: "ic_notif",
         importance: Importance.max,
       ),
       iOS: IOSNotificationDetails(),
     );
   }
 
-  static Future showNotifications({
+  static Future init({bool initscheduled = false}) async {
+    final android = AndroidInitializationSettings('ic_notif');
+    final ios = IOSInitializationSettings();
+    final settings = InitializationSettings(android: android, iOS: ios);
+    await _notifications.initialize(
+      settings,
+      onSelectNotification: (payload) async {
+        onNotifications.add(payload);
+      },
+    );
+    tz.initializeDatabase([]);
+  }
 
+  static Future showNotifications({
     int id = 0,
     String? title,
     String? body,
-  })async =>
+  }) async =>
       _notifications.show(
-          id,
-          title,
-          body,
-          await _notificationDetails(),
+        id,
+        title,
+        body,
+        await _notificationDetails(),
       );
+
+  static Future showtimedNotifications({
+    int id = 0,
+    String? title,
+    String? body,
+    String? payload,
+    required DateTime time,
+  }) async {
+    print(tz.TZDateTime.from(time, tz.local));
+    print("Called");
+
+    return _notifications.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(time, tz.local),
+      await _notificationDetails(),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
 }
