b03b46d5473d203c5ffce71ccbb23a9310d50927
sanika2001 <porayilsanika@gmail.com>
2022-09-23 00:43:21 +0530

    order confirmation backend done.

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: OrderScreen.id,
+        initialRoute: OnboardingScreen.id,
         routes: {
           OnboardingScreen.id: (context) => OnboardingScreen(),
           LoginScreen.id: (context) => LoginScreen(),
